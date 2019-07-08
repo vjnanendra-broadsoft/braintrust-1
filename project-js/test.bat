d2d9733a9e72509d191013a4b59168b298ca1469
Hari Sundararajan <harisundara.rajan@gmail.com>
2019-07-08 13:58:32 +0530

    Add notes on starting node in my Cygwin env

---
+++ project-js/test.bat
@@ -0,0 +1,5 @@
+cd "C:\Cygwin64\home\harisun\audio\braintrust\project-js"
+set path=%PATH%;"C:\Cygwin64\home\harisun\packages_install_HARISUN-QGFZR\node-v10.16.0-win-x64"
+npm install
+echo "Now run httpster"
+
